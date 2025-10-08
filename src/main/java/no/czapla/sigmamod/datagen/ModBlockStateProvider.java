3b44b1ba8d982b1820e9a6d34d9a51e107e65450
C <dawid.czaplicki2008@gmail.com>
2025-10-09 00:05:09 +0200

    Add block state and item model data providers
    Introduced ModBlockStateProvider and ModItemModelProvider for generating block states and item models in data generation. Updated DataGenerators to register these providers. Also added /src/generated to .gitignore.

--- src/main/java/no/czapla/sigmamod/datagen/ModBlockStateProvider.java
+++ src/main/java/no/czapla/sigmamod/datagen/ModBlockStateProvider.java
@@ -1,4 +1,25 @@
 package no.czapla.sigmamod.datagen;
 
-public class ModBlockStateProvider {
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import no.czapla.sigmamod.SigmaMod;
+import no.czapla.sigmamod.block.ModBlocks;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, SigmaMod.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.BLUE_STEEL_BLOCK);
+        blockWithItem(ModBlocks.MAGIC_BLOCK);
+        blockWithItem(ModBlocks.BLUE_STEEL_ORE);
+    }
+
+    private void blockWithItem(DeferredBlock<?> deferredBlock) {
+        simpleBlockWithItem(deferredBlock.get(), cubeAll(deferredBlock.get()));
+    }
 }
--- src/main/java/no/czapla/sigmamod/datagen/ModItemModelProvider.java
+++ src/main/java/no/czapla/sigmamod/datagen/ModItemModelProvider.java
@@ -1,4 +1,22 @@
 package no.czapla.sigmamod.datagen;
 
-public class ModItemModelProvider {
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import no.czapla.sigmamod.SigmaMod;
+import no.czapla.sigmamod.item.ModItems;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, SigmaMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        basicItem(ModItems.BLUE_STEEL.get());
+        basicItem(ModItems.RAW_BLUE_STEEL.get());
+        basicItem(ModItems.CHISEL.get());
+        basicItem(ModItems.DURIAN.get());
+        basicItem(ModItems.OIL_CAN.get());
+    }
 }
