1ab5a7673c56cf13b91025812252ecfe0692a159
C <dawid.czaplicki2008@gmail.com>
2025-10-08 23:56:07 +0200

    Add datagen providers and migrate resources to generated
    Introduces data generation classes for loot tables, block states, tags, item models, and recipes. Moves loot table JSONs to generated resources and removes manually maintained blockstate, model, recipe, and tag files from resources, transitioning to automated data generation.

---
+++ src/main/java/no/czapla/sigmamod/datagen/ModBlockLootTableProvider.java
@@ -0,0 +1,49 @@
+package no.czapla.sigmamod.datagen;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.loot.BlockLootSubProvider;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import no.czapla.sigmamod.block.ModBlocks;
+import no.czapla.sigmamod.item.ModItems;
+
+import java.util.Set;
+
+public class ModBlockLootTableProvider extends BlockLootSubProvider {
+    protected ModBlockLootTableProvider(HolderLookup.Provider registries) {
+        super(Set.of(), FeatureFlags.REGISTRY.allFlags(), registries);
+    }
+
+    @Override
+    protected void generate() {
+        dropSelf(ModBlocks.BLUE_STEEL_BLOCK.get());
+        dropSelf(ModBlocks.MAGIC_BLOCK.get());
+
+        add(ModBlocks.BLUE_STEEL_ORE.get(),
+                block -> createOreDrop(ModBlocks.BLUE_STEEL_ORE.get(), ModItems.RAW_BLUE_STEEL.get()));
+    }
+
+    // helper method for multiple ore drops
+    protected LootTable.Builder createMultipleOreDrops(Block pBlock, Item item, float minDrops, float maxDrops) {
+        HolderLookup.RegistryLookup<Enchantment> registrylookup = this.registries.lookupOrThrow(Registries.ENCHANTMENT);
+        return this.createSilkTouchDispatchTable(pBlock,
+                this.applyExplosionDecay(pBlock, LootItem.lootTableItem(item)
+                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(minDrops, maxDrops)))
+                        .apply(ApplyBonusCount.addOreBonusCount(registrylookup.getOrThrow(Enchantments.FORTUNE)))));
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return ModBlocks.BLOCKS.getEntries().stream().map(Holder::value)::iterator;
+    }
+}
---
+++ src/main/java/no/czapla/sigmamod/datagen/ModBlockStateProvider.java
@@ -0,0 +1,4 @@
+package no.czapla.sigmamod.datagen;
+
+public class ModBlockStateProvider {
+}
